26de9d93721585ff0cfe4b97daec1bfc2a5e073f
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-22 01:24:30 -0600

    Creacion de ejemplos especializacion

---
+++ src/main/java/com/mycompany/herenciaytipos/Ejemplos/Especializacion/Oftalmologo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.herenciaytipos.Ejemplos.Especializacion;
+
+/**
+ *
+ * @author nenod
+ */
+class Oftalmologo extends Medico {
+
+    void examenDeVista() {
+    System.out.println("El oftalmologo examina la vista del paciente");    
+    }
+    
+}
