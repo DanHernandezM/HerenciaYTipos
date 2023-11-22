4d87420476b6abe948a81321ca93ac49e9b1d4fd
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-21 23:32:56 -0600

    Inicio de la tarea

---
+++ src/main/java/com/mycompany/herenciaytipos/Ejemplos/Especificacion/HerenciaYTipos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herenciaytipos.Ejemplos.Especificacion;
+
+/**
+ *
+ * @author nenod
+ */
+public class HerenciaYTipos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
