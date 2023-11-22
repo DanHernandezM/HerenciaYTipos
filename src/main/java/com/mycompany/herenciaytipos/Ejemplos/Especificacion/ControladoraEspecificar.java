3c7d66073423e7ff46a53d3a4e26c774e300f4b0
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-21 23:37:08 -0600

    Creacion de controladoras

--- src/main/java/com/mycompany/herenciaytipos/Ejemplos/Especificacion/ControladoraEspecificar.java
+++ src/main/java/com/mycompany/herenciaytipos/Ejemplos/Especificacion/ControladoraEspecificar.java
@@ -8,7 +8,7 @@ package com.mycompany.herenciaytipos.Ejemplos.Especificacion;
  *
  * @author nenod
  */
-public class HerenciaYTipos {
+public class ControladoraEspecificar {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
